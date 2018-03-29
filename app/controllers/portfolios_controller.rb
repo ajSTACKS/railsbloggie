c729f53d2122d85fb0b30c7f35627c308e770f47
a j <>
2018-03-29 16:11:34 -0700

    created edit, new view for controller port

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,47 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  # def show
+  #
+  # end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio is live.' }
+
+      else
+        format.html { render :new }
+
+      end
+    end
+
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'the record was successfully updated.' }
+
+      else
+        format.html { render :edit }
+
+      end
+    end
+  end
+
+  end
+
+
+
 end
