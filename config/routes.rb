255f70e91175a0f6ff4b2bbfbf4ef653dfd091f7
a j <>
2018-03-29 20:07:57 -0700

    customize show routes to look cleaner

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
-    resources :blogs
+  resources :portfolios, except: [:show]
+  resources :blogs
 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#about'
 
