34ffb28467d0b9fbf1e3182504693e93f17b8b43
a j <>
2018-03-31 21:00:31 -0700

    integrated draft button to indicate published or draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  resources :blogs
+
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#about'
 
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root 'pages#home'
+
 
 end
