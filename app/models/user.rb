27f0c87c63e21ec945e30b9b80354b427c329ed2
a j <>
2018-04-06 00:19:38 -0700

    completed user authentication w/ virtual attr for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   # you have to be human email sent, locks the user out given the number of times logged try, time out a user by default, integrate third party logged in via fb twitt,telegram etc. you have to do customization in all of these
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
