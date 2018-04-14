06c3a8f01d55365a46a82db12a198c8530aa59d4
a j <>
2018-04-14 02:23:38 -0700

    adde custom layouts for blogs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+
+
+before_action :set_page_defaults
+
+  def set_page_defaults
+    @page_title = "AJ | Web Developer"
+    @seo_keywords = "blah"
+  end
 end
