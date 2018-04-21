dde280c2e164ea8d3d209b3a1e1f88ce76b65c23
a j <>
2018-04-21 15:53:29 -0700

    added petergate gem and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # you have to be human email sent, locks the user out given the number of times logged try, time out a user by default, integrate third party logged in via fb twitt,telegram etc. you have to do customization in all of these
