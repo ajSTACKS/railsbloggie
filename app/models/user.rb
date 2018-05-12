461152e9e0e51f7aad5cddf10392ad9716fd2002
a j <>
2018-05-11 22:07:46 -0700

    Implemented redis library and created commmnet generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :comments, dependent: :destroy
 
   validates_presence_of :name
 
